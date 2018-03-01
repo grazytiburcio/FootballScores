60f84289bb6e11fa550a2f79dd1c515bc66f3c1f
Grazyele Tiburcio <grazyeletiburcio@gmail.com>
2018-03-01 11:47:09 +0000

    Remove unnecessary lines

--- app/src/main/java/com/example/android/footballscores/MainActivity.java
+++ app/src/main/java/com/example/android/footballscores/MainActivity.java
@@ -2,42 +2,19 @@ package com.example.android.footballscores;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
-
 
 public class MainActivity extends AppCompatActivity {
     // Tracks the score for Ravens
     int scoreRavens = 0;
-
     // Tracks the score for Texans
     int scoreTexans = 0;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-
-        return super.onOptionsItemSelected(item);
     }
 
     /**
@@ -78,7 +55,6 @@ public class MainActivity extends AppCompatActivity {
     public void addTwoForRavens(View v) {
         scoreRavens = scoreRavens + 2;
         displayForRavens(scoreRavens);
-
     }
 
     /**
@@ -87,7 +63,6 @@ public class MainActivity extends AppCompatActivity {
     public void addTwoForTexans(View v) {
         scoreTexans = scoreTexans + 2;
         displayForTexans(scoreTexans);
-
     }
 
     /**
